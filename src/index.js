2683c3c8f1dc4a89378dbb516304411d634ae88a
kharchenkok <k.v.kharchenko@ukr.net>
2020-08-11 21:17:51 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -38,32 +38,79 @@ import './styles.css';
 // console.log(localStorage);
 // localStorage.removeItem('key')
 // ======================================================
-const checkbox = document.querySelector('.checkbox');
-const body = document.querySelector('body');
-// console.log(checkbox);
-let userThemeDark = 'checkbox-dark';
-let userThemeLight = 'checkbox-ligth';
+// const checkbox = document.querySelector('.checkbox');
+// const body = document.querySelector('body');
+// // console.log(checkbox);
+// let userThemeDark = 'checkbox-dark';
+// let userThemeLight = 'checkbox-ligth';
 
-// checkUserTheme();
-let changeTheme = function (e) {
-    console.log(e.target.checked);
-    if (e.target.checked) {
-        body.classList.add(userThemeDark);
-        localStorage.setItem('userTheme', 'userThemeDark');
-    } else {
-        body.classList.add(userThemeLight);
-        body.classList.remove(userThemeDark);
-        localStorage.setItem('userTheme', 'userThemeLight');
+// // checkUserTheme();
+// let changeTheme = function (e) {
+//     console.log(e.target.checked);
+//     if (e.target.checked) {
+//         body.classList.add(userThemeDark);
+//         localStorage.setItem('userTheme', 'userThemeDark');
+//     } else {
+//         body.classList.add(userThemeLight);
+//         body.classList.remove(userThemeDark);
+//         localStorage.setItem('userTheme', 'userThemeLight');
+//     }
+// };
+// function checkUserTheme() {
+//   let userCurrentTheme = localStorage.getItem('userTheme');
+//   console.log(userCurrentTheme);
+//   if (userCurrentTheme) {
+//     console.log(userCurrentTheme);
+//     body.classList.add(userCurrentTheme);
+//   }
+
+// };
+
+// checkbox.addEventListener('change', changeTheme);
+// ====================================================
+// const showInTime = function(t){
+// const setT = setTimeout(()=>{
+//   t+=100;
+//   if (t<1000){
+//     console.log('швидше', t);
+
+//   }else {
+//     console.log('ok', t);
+//   }
+//   return t<1500 ? showInTime(t) : ''
+// },t)
+// }
+// showInTime(0)
+// =========================================================
+const startBtn = document.querySelector('.start');
+const stopBtn = document.querySelector('.stop');
+const numbCounter = document.querySelector('.counter');
+console.log(startBtn);
+console.log(stopBtn);
+console.log(numbCounter);
+let counter = 0;
+let timer;
+const startCounter = function () {
+  timer = setInterval(() => {
+    counter += 1;
+    numbCounter.textContent = counter;
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+  }, 1000);
+};
+
+const stopCounter = function (e) {
+  clearInterval(timer);
+  startBtn.disabled = false;
+  numbCounter.textContent = counter;
+    stopBtn.disabled = false;
+    
+    if ((e.target.textContent = 'reset')) {
+      
+      numbCounter.textContent = 0;
     }
-};
-function checkUserTheme() {
-  let userCurrentTheme = localStorage.getItem('userTheme');
-  console.log(userCurrentTheme);
-  if (userCurrentTheme) {
-    console.log(userCurrentTheme);
-    body.classList.add(userCurrentTheme);
-  }
- 
+    stopBtn.textContent = 'reset';
 };
 
-checkbox.addEventListener('change', changeTheme);
+startBtn.addEventListener('click', startCounter);
+stopBtn.addEventListener('click', stopCounter);
