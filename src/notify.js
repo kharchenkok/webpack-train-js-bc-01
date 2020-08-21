c523c083f19b106760d21f9026bc35b84a50c2d6
kharchenkok <k.v.kharchenko@ukr.net>
2020-08-21 14:02:29 +0300

    commit

--- src/notify.js
+++ src/notify.js
@@ -1,55 +1,55 @@
-// import { alert } from '@pnotify/core';
-import { alert, notice, info, success, error } from '@pnotify/core';
-// import '@pnotify/core/dist/BrightTheme.css';
-// import '@pnotify/core/dist/Material.css';
-// import { defaults } from '@pnotify/core';
+// // import { alert } from '@pnotify/core';
+// import { alert, notice, info, success, error } from '@pnotify/core';
+// // import '@pnotify/core/dist/BrightTheme.css';
+// // import '@pnotify/core/dist/Material.css';
+// // import { defaults } from '@pnotify/core';
 
-// import { info } from "@pnotify/core";
-import "@pnotify/core/dist/PNotify.css";
-import "@pnotify/core/dist/BrightTheme.css";
-import * as Confirm from "@pnotify/confirm";
-import "@pnotify/confirm/dist/PNotifyConfirm.css";
+// // import { info } from "@pnotify/core";
+// import "@pnotify/core/dist/PNotify.css";
+// import "@pnotify/core/dist/BrightTheme.css";
+// import * as Confirm from "@pnotify/confirm";
+// import "@pnotify/confirm/dist/PNotifyConfirm.css";
 
-function click() {
+// function click() {
 
-    error({
-        title: 'Oh No!',
-        text: 'Something terrible happened.'
-      });
-  info({
-    title: "Button Clicked",
-    text:
-      "You have clicked the button. You may now complete the process of reading the notice.",
-    modules: new Map([
-      [
-        Confirm,
-        {
-          confirm: true,
-          buttons: [
-            {
-              text: "Ok",
-              primary: true,
-              click: notice => {
-                notice.close();
-              }
-            }
-          ]
-        }
-      ]
-    ])
-  });
-}
+//     error({
+//         title: 'Oh No!',
+//         text: 'Something terrible happened.'
+//       });
+//   info({
+//     title: "Button Clicked",
+//     text:
+//       "You have clicked the button. You may now complete the process of reading the notice.",
+//     modules: new Map([
+//       [
+//         Confirm,
+//         {
+//           confirm: true,
+//           buttons: [
+//             {
+//               text: "Ok",
+//               primary: true,
+//               click: notice => {
+//                 notice.close();
+//               }
+//             }
+//           ]
+//         }
+//       ]
+//     ])
+//   });
+// }
 
-const App = document.getElementById("app");
+// const App = document.getElementById("app");
 
-App.innerHTML = `
-<div class="container">
-  <h1>PNotify 5 in Vanilla ES6!</h1>
-  <button>Notify me!</button>
-</div>
-`;
+// App.innerHTML = `
+// <div class="container">
+//   <h1>PNotify 5 in Vanilla ES6!</h1>
+//   <button>Notify me!</button>
+// </div>
+// `;
 
-App.querySelector("button").addEventListener("click", click);
+// App.querySelector("button").addEventListener("click", click);
 
 
 
